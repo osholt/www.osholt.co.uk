515c042968646518c798351b83fa37d1240b02a7
Oliver Holt <osholt@Server1.home>
2016-08-28 18:12:56 +0100

    Added all files

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php define('root', "/var/www/www.osholt.co.uk/"); ?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+	<?php include root .'/Includes/head.html';?>
+	<body>
+		<?php include root .'/Includes/nav.html';?>
+		<div class="main-container">
+         		<div class="main wrapper clearfix">
+				<?php include 'article.html';?>
+				<?php include root .'/Includes/contact.html';?>
+			</div> <!-- #main -->
+		</div> <!-- #main-container -->
+		<?php include root .'/Includes/footer.html';?>
+	</body>
+    <!-- Thanks for taking the time to audit my code! -->
+    <!-- In case anyone asks, all the photos hosted on on this site belong to me and are licenced along with the rest of my stuff. -->
+    <!-- I use cookies for Google Analytics. I know I'm not in compliance. Fine me.-->
+</html>
